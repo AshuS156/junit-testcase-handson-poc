a840f19033f8f6a69769fdf307c1d5f085edbcdd
AshuS156 <sashu2752@gmail.com>
2025-11-17 19:26:11 +0530

    initial commit

---
+++ unit-testcase-poc/unit-testcase-poc/src/main/java/com/pathfinder/UnitTestcasePocApplication.java
@@ -0,0 +1,13 @@
+package com.pathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestcasePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestcasePocApplication.class, args);
+	}
+
+}
